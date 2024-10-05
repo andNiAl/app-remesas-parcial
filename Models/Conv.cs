075d0a8a6bdeba5dec5775f898139e3af5806a2e
andNiAl <andrea_alva2@usmp.pe>
2024-10-04 23:40:10 -0500

    cambios

--- Models/Conv.cs
+++ Models/Conv.cs
@@ -1,12 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace app_remesas_parcial.Models
 {
-    public class Conv
+    [Table("t_conversion")]
+    public class Conversion
     {
-        
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public decimal? Monto { get; set; }
+        public string? MonedaOrigen { get; set; }
+
     }
 }
