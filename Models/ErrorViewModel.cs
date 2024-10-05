1027a6f9ada0defe9a936e76311983a4f3979ae4
andNiAl <andrea_alva2@usmp.pe>
2024-10-04 21:38:01 -0500

    e

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Aplicaci_n_de_Remesas_Internacionales_con_Gesti_n_de_Transacciones_en_D_lares_y_Bitcoin.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
