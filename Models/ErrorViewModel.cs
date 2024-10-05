355434918e34ff9314d3d6b4b6ce1da4607af914
andNiAl <andrea_alva2@usmp.pe>
2024-10-04 21:38:51 -0500

    creacion

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace app_remesas_parcial.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
